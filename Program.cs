96ea7c856b6d53f6b8d294117644a2be710554fc
vadim <rozhevadim@gmail.com>
2023-02-07 19:41:10 +0700

    end

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num; 
+Console.Write("Enter nimber: ");
+num = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Your number: " + num);
+ 
+Console.Read();
+       
+Console.WriteLine("Number\tcube number");
+
+for (int i = 0; i <= num; i++)
+
+{Console.WriteLine("{0}\t{1}", i, Math.Pow(i,2), Math.Pow(i, 3));
+
+}
