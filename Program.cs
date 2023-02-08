0a7a348659c6d4a1a1c182ccb740c134c2262c11
vadim <rozhevadim@gmail.com>
2023-02-08 09:58:33 +0700

    delete 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿int num; 
+﻿// Задание 23-1
+
+int num;
+
 Console.Write("Enter nimber: ");
+
 num = Convert.ToInt32 (Console.ReadLine());
+
 Console.WriteLine("Your number: " + num);
  
 Console.Read();
@@ -9,6 +14,6 @@ Console.WriteLine("Number\tcube number");
 
 for (int i = 0; i <= num; i++)
 
-{Console.WriteLine("{0}\t{1}", i, Math.Pow(i,2), Math.Pow(i, 3));
+{Console.WriteLine("{0}\t{1}", i, Math.Pow(i,3));
 
 }
